a6f3ad3a35c99894e7da7acda2a07691eff97789
tinonino <aresfrend13@gmail.com>
2019-03-03 15:54:58 +0200

    favicon

--- game.js
+++ game.js
@@ -1,7 +1,25 @@
 const { app, BrowserWindow } = require('electron')
+var path = require('path')
 
 function createWindow(){
-	let win=new BrowserWindow({width: 1000, heght: 1000});
+	let win=new BrowserWindow({
+		width: 1000, 
+		heght: 1000,
+		minWidth: 1281,
+     	minHeight: 800,
+     	backgroundColor: '#312450',
+	    icon: path.join(__dirname, 'icons/favicon-32x32.png')
+	});
 	win.loadFile('index.html');
+
+	win.on('closed', function () {
+       // Dereference the window object, usually you would store windows
+       // in an array if your app supports multi windows, this is the time
+       // when you should delete the corresponding element.
+       win = null
+    })
+
+    require('./menu/mainmenu')
+
 }
 app.on('ready', createWindow);
